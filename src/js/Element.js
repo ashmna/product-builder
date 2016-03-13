6ed62a154993351d789d1fd0fd8a209ecd9146c9
ashmna <mnatsakanyan.ashot@gmail.com>
2016-03-13 21:01:51 +0400

    Resize, Move and Rotate function move into private section.

--- src/js/Element.js
+++ src/js/Element.js
@@ -19,8 +19,6 @@ function Element(broker, borders, position, params)
 
   // Public Methods
 
-  that.init = function () {};
-
   that.getContext = function () {
     return that.context;
   };
@@ -58,7 +56,7 @@ function Element(broker, borders, position, params)
 
   that.getCoordinates = function () {
     var piHalf = Math.PI * 0.5
-      , sin = Math.sin(that.position.angle % piHalf)
+      , sin = Math.sin(Math.abs(that.position.angle) % piHalf)
       , p = that.position
       , w = parseInt(that.position.angle / piHalf) % 2 ? p.height : p.width
       , h = parseInt(that.position.angle / piHalf) % 2 ? p.width  : p.height
@@ -71,6 +69,67 @@ function Element(broker, borders, position, params)
       bl : { x:     p.x + deltaX, y : h + p.y - deltaY },
       br : { x: w + p.x - deltaX, y : h + p.y + deltaY }
     };
+  };
+
+  that.initDraggable = function(object) {
+    object = object || {};
+    that.context.append(
+      $('<div class="helper-point helper-move"> <em class="fa fa-arrows"></em> </div>')
+    );
+    return that.context.draggable($.extend({
+      start: function(event) {
+        x = event.pageX - that.position.x;
+        y = event.pageY - that.position.y;
+      },
+      drag: function(event, ui) {
+        that.position.x = event.pageX - x;
+        that.position.y = event.pageY - y;
+        ui.position.left = that.position.x;
+        ui.position.top  = that.position.y;
+        that.setOutOfBorder(!checkInBorder());
+        that.publish('drag', [event, ui]);
+      }
+    }, object));
+  };
+
+  that.initResizable = function(object) {
+    object = object || {};
+    var helper = $('<div class="helper-point helper-resize"> <em class="fa fa-expand fa-flip-horizontal"></em> </div>');
+    that.context.append(helper);
+    return that.context.resizable($.extend({
+      handles: {'se': helper },
+      resize: function(event, ui) {
+        that.position.width  = ui.size.width;
+        that.position.height = ui.size.height;
+        that.setOutOfBorder(!checkInBorder());
+        that.publish('resize', [event, ui]);
+      }
+    }, object));
+  };
+
+  that.initRotatable = function(object) {
+    object = object || {};
+    var helper = $('<div class="helper-point helper-rotate"> <em class="fa fa-undo"></em> </div>');
+    that.context.append(helper);
+    var rotatable = that.context.rotatable($.extend({
+      angle: that.position.angle,
+      handle: helper,
+      rotate: function (event, ui) {
+        that.position.angle = ui.angle.current;
+        that.setOutOfBorder(!checkInBorder());
+        that.publish('rotate', [event, ui]);
+      }
+    }, object));
+    that.context.unbind('wheel');
+    return rotatable;
+  };
+
+  that.initDeletable = function() {
+    var helper = $('<div class="helper-point helper-remove"> <em class="fa fa-trash"></em> </div>');
+    that.context.append(helper);
+    return helper.click(function(){
+      that.broker.detach(that);
+    });
   };
 
   // Private Methods
@@ -103,13 +162,7 @@ function Element(broker, borders, position, params)
   }
 
   function initElement() {
-    that.context = $('\
-      <div class="element">\
-        <div class="helper-point helper-move"  > <em class="fa fa-arrows"></em>                    </div>\
-        <div class="helper-point helper-rotate"> <em class="fa fa-undo"></em>                      </div>\
-        <div class="helper-point helper-remove"> <em class="fa fa-trash"></em>                     </div>\
-        <div class="helper-point helper-resize"> <em class="fa fa-expand fa-flip-horizontal"></em> </div>\
-      </div>')
+    that.context = $('<div class="element"></div>')
       .css({
         left  : that.position.x,
         top   : that.position.y,
@@ -121,61 +174,8 @@ function Element(broker, borders, position, params)
     });
   }
 
-  function initDraggable() {
-    that.context.draggable({
-      start: function(event) {
-        x = event.pageX - that.position.x;
-        y = event.pageY - that.position.y;
-      },
-      drag: function(event, ui) {
-        that.position.x = event.pageX - x;
-        that.position.y = event.pageY - y;
-        ui.position.left = that.position.x;
-        ui.position.top  = that.position.y;
-        that.setOutOfBorder(!checkInBorder());
-        that.publish('drag', [event, ui]);
-      }
-    });
-  }
-
-  function initResizable() {
-    that.context.resizable({
-      handles: {'se': that.context.find('.helper-resize')},
-      resize: function(event, ui) {
-        that.position.width  = ui.size.width;
-        that.position.height = ui.size.height;
-        that.setOutOfBorder(!checkInBorder());
-        that.publish('resize', [event, ui]);
-      }
-    });
-  }
-
-  function initRotatable() {
-    that.context.rotatable({
-      angle: that.position.angle,
-      handle: that.context.find('.helper-rotate'),
-      rotate: function (event, ui) {
-        that.position.angle = ui.angle.current;
-        that.setOutOfBorder(!checkInBorder());
-        that.publish('rotate', [event, ui]);
-      }
-    });
-    that.context.unbind('wheel');
-  }
-
-  function initDeletable() {
-    that.context.find('.helper-remove').click(function(){
-      that.broker.detach(that);
-    });
-  }
-
   // Constructor
 
   initElement();
-  initResizable();
-  initRotatable();
-  initDraggable();
-  initDeletable();
-  that.init();
   that.broker.append(that);
 }
