7c4e376ffe38a0853a3c85244df144015629a49f
ashmna <mnatsakanyan.ashot@gmail.com>
2016-03-13 21:02:21 +0400

    Added select text jquery plugin.

---
+++ src/js/lib/select-text.js
@@ -0,0 +1,20 @@
+(function ($) {
+
+  $.fn.selectText = function () {
+    var element = this[0]
+      , range
+      ;
+    if (document.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(element);
+      range.select();
+    } else if (window.getSelection) {
+      var selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(element);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  };
+
+}(jQuery));
